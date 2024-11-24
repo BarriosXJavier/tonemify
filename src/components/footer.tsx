27a59b1bb84d2737963ce91832007a073b0d832e
David Muriithi <88942514+BarriosXJavier@users.noreply.github.com>
2024-11-24 12:06:21 +0000

    footer adjustments

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,18 +4,21 @@ import Image from "next/image";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="font-mono bg-background py-10 sm:py-12 px-6 md:px-16 lg:px-24  pt-8 mt-8 text-center border-t border-muted">
-        <p className="text-muted-foreground text-xs sm:text-sm md:text-base flex items-center gap-2 justify-center">
-          &copy; TonemifyLabs {new Date().getFullYear()}. All rights reserved.{" "}
-          <Link
-            href="https://x.com/barrios__x"
-            className="flex items-center gap-1"
-            target="_blank noopener"
-          >
-            <Image src="/x.png" alt="x logo" width={20} height={20} />
-            barriös__x
-          </Link>
-        </p>    
+    <footer className="font-mono bg-background py-10 sm:py-12 px-6 md:px-16 lg:px-24 pt-8 mt-8 text-center border-t border-muted">
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-muted-foreground text-xs sm:text-sm md:text-base">
+          &copy; TonemifyLabs {new Date().getFullYear()}. All rights reserved.
+        </p>
+        <Link
+          href="https://x.com/barrios__x"
+          className="flex items-center gap-2 text-muted-foreground text-xs sm:text-sm md:text-base hover:text-foreground transition-colors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/x.png" alt="x logo" width={20} height={20} />
+          <span>barriös__x</span>
+        </Link>
+      </div>
     </footer>
   );
 };
