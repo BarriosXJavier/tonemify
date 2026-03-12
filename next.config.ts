464bf08559ca765a93f06b02f82439b7b45e205a
David Muriithi <muriithid05@gmail.com>
2026-03-12 11:04:41 +0300

    fixup

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.watchOptions = {
-      ignored: /node_modules/,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
